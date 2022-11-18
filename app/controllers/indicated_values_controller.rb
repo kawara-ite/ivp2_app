6b38faf80124d778606730dcf82313b3d87b2ee6
kawara-ite <114914557+kawara-ite@users.noreply.github.com>
2022-11-18 11:55:04 +0900

    routes indicated_values index new create

--- app/controllers/indicated_values_controller.rb
+++ app/controllers/indicated_values_controller.rb
@@ -1,4 +1,10 @@
 class IndicatedValuesController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'indicated_values/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'indicated_values#index'
+  resources :indicated_values, only: [:index, :create, :new]
 end
