3975db18a49dd0e53f2ad1d2b69e237d6c3afaf1
kawara-ite <114914557+kawara-ite@users.noreply.github.com>
2022-11-18 12:40:19 +0900

    post success

--- app/controllers/indicated_values_controller.rb
+++ app/controllers/indicated_values_controller.rb
@@ -3,8 +3,21 @@ class IndicatedValuesController < ApplicationController
   end
 
   def new
+    @indicated_value = IndicatedValue.new
   end
 
   def create
+    @indicated_value = IndicatedValue.new(indicated_value_params)
+    if @indicated_value.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def indicated_value_params
+    params.require(:indicated_value).permit(:equipment_number, :instrument_type, :instruction)
   end
 end
--- app/models/indicated_value.rb
+++ app/models/indicated_value.rb
@@ -1,2 +1,8 @@
 class IndicatedValue < ApplicationRecord
+
+  with_options presence: true do
+    validates :equipment_number
+    validates :instrument_type
+    validates :instruction
+  end
 end
