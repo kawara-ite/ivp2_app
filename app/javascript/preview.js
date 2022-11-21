1e4f3bd6ff338f40309e2f6804a3b3f2140617fe
kawara-ite <114914557+kawara-ite@users.noreply.github.com>
2022-11-21 19:51:00 +0900

    追加

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
  
       if(window.confirm('サーバーから画像を削除します。\nよろしいですか？'))
       {
-         $(this).parent().find('#equipment-image').val('');
+         $(this).parent().find('#equipment-image-file').val('');
          $(this).parent().find('.img_view, br').remove();
          $(this).remove();
       }
@@ -68,7 +68,7 @@ $(document).ready(function () {
  
       if(window.confirm('サーバーから画像を削除します。\nよろしいですか？'))
       {
-         $(this).parent().find('#equipment-image').val('');
+         $(this).parent().find('#equipment-image-camera').val('');
          $(this).parent().find('.img_view, br').remove();
          $(this).remove();
       }
@@ -79,9 +79,9 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  var view_box = $('#instruction-view_box');
+  var view_box = $('#instruction-file-view_box');
    
-  $("#instruction-image").on('change', function(){
+  $("#instruction-image-file").on('change', function(){
      var fileprop = $(this).prop('files')[0],
          find_img = $(this).next('img'),
          fileRdr = new FileReader();
@@ -108,7 +108,47 @@ $(document).ready(function () {
  
       if(window.confirm('サーバーから画像を削除します。\nよろしいですか？'))
       {
-         $(this).parent().find('#instruction-image').val('');
+         $(this).parent().find('#instruction-image-file').val('');
+         $(this).parent().find('.img_view, br').remove();
+         $(this).remove();
+      }
+ 
+      return false;
+    });
+  }  
+});
+
+$(document).ready(function () {
+  var view_box = $('#instruction-camera-view_box');
+   
+  $("#instruction-image-camera").on('change', function(){
+     var fileprop = $(this).prop('files')[0],
+         find_img = $(this).next('img'),
+         fileRdr = new FileReader();
+     
+     if(find_img.length){
+        find_img.nextAll().remove();
+        find_img.remove();
+     }
+     
+    var img = '<img width="200" alt="" class="img_view"><a href="#" class="img_del">画像を削除する</a>';
+ 
+    view_box.append(img);
+     
+    fileRdr.onload = function() {    
+      view_box.find('img').attr('src', fileRdr.result);
+      img_del(view_box); 
+    }
+    fileRdr.readAsDataURL(fileprop);  
+  });
+   
+  function img_del(target)
+  {
+     target.find("a.img_del").on('click',function(){
+ 
+      if(window.confirm('サーバーから画像を削除します。\nよろしいですか？'))
+      {
+         $(this).parent().find('#instruction-image-camera').val('');
          $(this).parent().find('.img_view, br').remove();
          $(this).remove();
       }
