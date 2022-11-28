826efc5e5cb38fa1ba83a1aeef6732e6adaf7755
kawara-ite <114914557+kawara-ite@users.noreply.github.com>
2022-11-28 15:20:10 +0900

    S3 introduction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV["BASIC_AUTH_USER_2"] && password == ENV["BASIC_AUTH_PASSWORD_2"]
     end
   end
 end
